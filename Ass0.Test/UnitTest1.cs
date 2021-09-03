250180cde1f4632ca2036903af5a19441749c45c
asmt <asmt@itu.dk>
2021-09-03 15:26:25 +0200

    Tested and implemented IsLeapYear.

--- Ass0.Test/UnitTest1.cs
+++ Ass0.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Ass0.Test
@@ -6,9 +7,27 @@ namespace Ass0.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
+        public void LeapYear_IsLeapYear(){
 
+            var out2004 = LeapYear.IsLeapYear(2004);
+            var out1996 = LeapYear.IsLeapYear(1996);
+            var out1997 = LeapYear.IsLeapYear(1997);
+            
+            var out1700 = LeapYear.IsLeapYear(1700);
+            var out1800 = LeapYear.IsLeapYear(1800);
+            var out1900 = LeapYear.IsLeapYear(1900);
+            var out2000 = LeapYear.IsLeapYear(2000);
+
+            Assert.Equal(true,  out2004);
+            Assert.Equal(true,  out1996);
+            Assert.Equal(false, out1997);
+            
+            Assert.Equal(false, out1700);
+            Assert.Equal(false, out1800);
+            Assert.Equal(false, out1900);
+            Assert.Equal(true,  out2000);
+            
         }
+        
     }
 }
---
+++ Ass0/LeapYear.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ass0{
+
+    public static class LeapYear{
+
+        public static bool IsLeapYear(int year){
+            return year%4 == 0 && (year%100 != 0 || year%400 == 0);
+        }
+
+    }
+
+}
