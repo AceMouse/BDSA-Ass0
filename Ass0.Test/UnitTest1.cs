67fba27e317d2645ad3c4faedac340a0420f8cb5
AceMouse <62022703+AceMouse@users.noreply.github.com>
2021-09-03 15:42:28 +0200

    Feature/leap year (#2)
    * add console app and test lib
    
    * Tested and implemented IsLeapYear.

--- Ass0.Test/UnitTest1.cs
+++ Ass0.Test/UnitTest1.cs
@@ -3,12 +3,29 @@ using Xunit;
 
 namespace Ass0.Test
 {
-    public class UnitTest1
+    public class LeapYearTest
     {
         [Fact]
-        public void Test1()
-        {
+        public void LeapYear_IsLeapYear(){
 
+            var out2004 = LeapYear.IsLeapYear(2004);
+            var out1996 = LeapYear.IsLeapYear(1996);
+            var out1997 = LeapYear.IsLeapYear(1997);
+            
+            var out1700 = LeapYear.IsLeapYear(1700);
+            var out1800 = LeapYear.IsLeapYear(1800);
+            var out1900 = LeapYear.IsLeapYear(1900);
+            var out2000 = LeapYear.IsLeapYear(2000);
+
+            Assert.True(out2004);
+            Assert.True(out1996);
+            Assert.False(out1997);
+            
+            Assert.False(out1700);
+            Assert.False(out1800);
+            Assert.False(out1900);
+            Assert.True(out2000);
+            
         }
     }
 }
